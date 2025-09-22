51796ff457c52de44f1a8aa3bb13a8e3530395d5
Choungion <leefuentespablo.1@hotmail.es>
2025-09-22 14:58:33 -0600

    General Changes

---
+++ .config/hypr/scripts/gamemodeon.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+hyprctl keyword monitor DP-1, 1920x1080@165, 1920x2000, 1, transform, 3
---
+++ .config/hypr/scripts/monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Launch htop in a new kitty terminal and store its PID
+kitty --title "htop_float" -e htop &
+HTOP_PID=$!
+
+# Launch nvtop in a new kitty terminal and store its PID
+kitty --title "nvtop_float" -e nvtop &
+NVTOP_PID=$!
+
+# Wait for either process to terminate
+while ps -p $HTOP_PID > /dev/null && ps -p $NVTOP_PID > /dev/null; do
+    sleep 0.5
+done
+
+# If htop is no longer running, kill nvtop
+if ! ps -p $HTOP_PID > /dev/null; then
+    kill $NVTOP_PID
+fi
+
+# If nvtop is no longer running, kill htop
+if ! ps -p $NVTOP_PID > /dev/null; then
+    kill $HTOP_PID
+fi
